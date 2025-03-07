ccf596ad12630c05dee05e4e671e0ab9dfbfeaed
Андрей Лещенко <peperonnka@mail.ru>
2025-03-07 19:37:47 +0300

    add files for 4 lab
    (Zapp.run)

---
+++ lab4/Lab4/lib/main.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
+import 'package:flutter/services.dart';
+
+// Условный импорт для dart:html
+import 'dart:js' as js;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Общежития КубГАУ',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: DormitoryDetailsScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class DormitoryDetailsScreen extends StatefulWidget {
+  @override
+  _DormitoryDetailsScreenState createState() => _DormitoryDetailsScreenState();
+}
+
+class _DormitoryDetailsScreenState extends State<DormitoryDetailsScreen> {
+  bool isFavorite = false;
+  int likes = 0;
+
+  String longText =
+      "Студенческий городок или так называемый кампус Кубанского ГАУ состоит из двадцати общежитий, в которых проживает более 8000 студентов, что составляет 96% от всех нуждающихся. Студенты первого курса обеспечены местами в общежитии полностью. В соответствии с Положением о студенческих общежитиях университета, при поселении между администрацией и студентами заключается договор найма жилого помещения. Воспитательная работа в общежитиях направлена на улучшение быта, соблюдение правил внутреннего распорядка, отсутствия асоциальных явлений в молодежной среде. Условия проживания в общежитиях университетского кампуса полностью отвечают санитарным нормам и требованиям: наличие оборудованных кухонь, душевых комнат, прачечных, читальных залов, комнат самоподготовки, помещений для заседаний студенческих советов и наглядной агитации. С целью улучшения условий быта студентов активно работает система студенческого самоуправления - студенческие советы организуют всю работу по самообслуживанию.";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Общежития КубГАУ'),
+        backgroundColor: Colors.green,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              'pic.jpg',
+              width: double.infinity,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Общежитие №20',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text('Краснодар, ул. Калинина, 13'),
+                  SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          isFavorite ? Icons.favorite : Icons.favorite_border,
+                          color: Colors.red,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            isFavorite = !isFavorite;
+                            if (isFavorite) {
+                              likes++;
+                            } else {
+                              likes--;
+                            }
+                          });
+                        },
+                      ),
+                      Text('$likes'),
+                    ],
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        _buildActionButton(
+                          icon: Icons.call,
+                          text: 'Позвонить',
+                          onPressed: () => _copyToClipboard(context, '+7 (918) ххх хх-хх', 'Номер телефона скопирован:'),
+                        ),
+                        _buildActionButton(
+                          icon: Icons.map,
+                          text: 'Маршрут',
+                          onPressed: () => _copyToClipboard(context, 'Краснодар, ул. Калинина, 13', 'Маршрут скопирован:'),
+                        ),
+                        _buildActionButton(
+                          icon: Icons.share,
+                          text: 'Поделиться',
+                          onPressed: () => _copyToClipboard(context, 'https://zapp.run/', 'Ссылка на приложение скопирована:'),
+                        ),
+                      ],
+                    ),
+                  SizedBox(height: 16),
+                  Text(
+                    longText,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildActionButton({
+    required IconData icon,
+    required String text,
+    required VoidCallback onPressed,
+  }) {
+    return TextButton.icon(
+      icon: Icon(icon, color: Colors.green),
+      label: Text(text, style: TextStyle(color: Colors.green)),
+      onPressed: onPressed,
+    );
+  }
+  Future<void> _copyToClipboard(BuildContext context, String text, String message) async {
+    await Clipboard.setData(ClipboardData(text: text));
+    _showInfoDialog(context, message, text);
+  }
+
+  void _showInfoDialog(BuildContext context, String title, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+}
